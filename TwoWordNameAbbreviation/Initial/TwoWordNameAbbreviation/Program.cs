05001af2187ddd826ae88ef8a04c5a8265485f3d
Sam Wells <samwells95@gmail.com>
2019-02-05 17:50:31 +0000

    add folder structure and initial project

---
+++ TwoWordNameAbbreviation/Initial/TwoWordNameAbbreviation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoWordNameAbbreviation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's get those names");
+        }
+    }
+}
