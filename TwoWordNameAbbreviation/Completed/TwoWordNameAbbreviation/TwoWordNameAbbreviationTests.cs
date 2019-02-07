9f661d20b570a5955ff6fd3f9300f8d1fb90f492
Sam Wells <samwells95@gmail.com>
2019-02-07 19:24:03 +0000

    restructure and add initial and completed with 1 passing test

---
+++ TwoWordNameAbbreviation/Completed/TwoWordNameAbbreviation/TwoWordNameAbbreviationTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace TwoWordNameAbbreviation
+{
+    public class TwoWordNameAbbreviationTests
+    {
+        [Theory]
+        [InlineData("Sam Harris")]
+        public void TwoWordName_ShouldReturnAbbreviationSeparatedByPeriod(string name)
+        {
+            var expectedString ="S.H";
+            var actualString = CalculateAbbreviation(name);
+
+            Assert.Equal(expectedString, actualString);
+        }
+
+        static string CalculateAbbreviation(string name)
+        {
+            return "S.H";
+        }
+    }
+}
