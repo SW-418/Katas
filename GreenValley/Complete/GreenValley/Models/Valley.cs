8bf8892b3bf40a4cd9c20ef8f383b10ecda3c4d4
Sam Wells <samwells95@gmail.com>
2019-02-09 18:04:44 +0000

    Add completed kata

---
+++ GreenValley/Complete/GreenValley/Models/Valley.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GreenValley
+{
+    public class Valley
+    {
+        public static int[] MakeValley(int[] arr){
+            var valleyWithWings = new int[arr.Length];
+            var firstIndex = 0;
+            var secondIndex = 1;
+            
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if(i % 2 == 0){
+                    valleyWithWings[firstIndex] = arr[i];
+                    firstIndex++;
+                }
+                else{
+                    valleyWithWings[arr.Length - secondIndex] = arr[i];
+                    secondIndex++;
+                }
+            }
+            return valleyWithWings;
+        }
+    }
+}
