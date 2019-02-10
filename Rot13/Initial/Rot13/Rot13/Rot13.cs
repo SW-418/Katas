79040695e2f6ee81e58576ea1738d747a84f6c68
Sam Wells <samwells95@gmail.com>
2019-02-10 11:32:30 +0000

    Add initial for Rot13 Kata

---
+++ Rot13/Initial/Rot13/Rot13/Rot13.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Rot13
+{
+    public class Rot13
+    {
+        public static List<string> LetterList = new List<string>{ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+        
+        public static string ConvertMessage(string message)
+        {
+            return message;
+        }
+    }
+}
