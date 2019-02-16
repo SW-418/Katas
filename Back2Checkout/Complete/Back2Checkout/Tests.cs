ad9d4095e1bbbe8d96140ee0f639bed9370a8580
Sam Wells <samwells95@gmail.com>
2019-02-16 20:51:31 +0000

    Add solution for completed kata

---
+++ Back2Checkout/Complete/Back2Checkout/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Back2Checkout
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(new Item("A", 10.0))]
+        public void Checkout_ShouldReturnPriceOfItem(string sku, double price)
+        {
+            var shoppingItem = new Item(sku, price);
+            var expectedResult = shoppingItem.Price;
+            var actualResult = CheckoutSystem.Checkout(new List<Item>{shoppingItem});
+            
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
