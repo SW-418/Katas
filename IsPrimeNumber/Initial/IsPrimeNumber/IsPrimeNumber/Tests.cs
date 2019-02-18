757ab919aa758f2a535998ea4a84194c619a4609
Sam Wells <samwells95@gmail.com>
2019-02-18 20:14:46 +0000

    Add initial project with failing tests

---
+++ IsPrimeNumber/Initial/IsPrimeNumber/IsPrimeNumber/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace IsPrimeNumber
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, false)]
+        public void IsPrime_ShouldReturnFalseIfNotPrime(int n, bool expectedValue)
+        {
+            var actualResult = PrimeCalculator.IsPrimeNumber(n);
+            Assert.Equal(expectedValue, actualResult);
+        }
+        
+//        [Theory]
+//        [InlineData(2, true)]
+//        public void IsPrime_ShouldReturnTrueIfPrime(int n, bool expectedValue)
+//        {
+//            var actualResult = PrimeCalculator.IsPrimeNumber(n);
+//            Assert.Equal(expectedValue, actualResult);
+//        }
+    }
+}
