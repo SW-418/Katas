4a0d6b69050620d911a693f7a8483c7f3b4b35f6
Sam Wells <samwells95@gmail.com>
2019-02-19 19:52:24 +0000

    Add new test and logic

--- Meeting/Complete/Meeting/Meeting/MeetingOrganiser.cs
+++ Meeting/Complete/Meeting/Meeting/MeetingOrganiser.cs
@@ -1,12 +1,38 @@
+using System;
+using System.Text;
+
 namespace Meeting
 {
     public static class MeetingOrganiser
     {
         public static string CreateMeeting(string names)
         {
+            var stringbuilder = new StringBuilder();
+            stringbuilder.Append("(");
             var splitNames = names.Split(':');
-            var newName = splitNames[0].ToUpper() + " " + splitNames[1].ToUpper();
-            return newName;
+            var newName = splitNames[0] + " " + splitNames[1];
+
+            var capitalisedName = CapitaliseName(newName);
+            var reversedName = ReverseNames(capitalisedName);
+
+            var formattedName = reversedName.Replace(" ", ", ");
+
+            stringbuilder.Append(formattedName);
+            stringbuilder.Append(")");
+               
+            return stringbuilder.ToString();
+        }
+
+        public static string CapitaliseName(string name)
+        {
+            var splitName = name.Split(' ');
+            return splitName[0].ToUpper() + " " + splitName[1].ToUpper();
+        }
+
+        public static string ReverseNames(string name)
+        {
+            var splitName = name.Split(' ');
+            return splitName[1] + " " + splitName[0];
         }
     }
 }
--- Meeting/Complete/Meeting/Meeting/Tests.cs
+++ Meeting/Complete/Meeting/Meeting/Tests.cs
@@ -4,29 +4,45 @@ using Xunit;
 namespace Meeting
 {
     public class Tests
-    {
+    {   
         [Theory]
-        [InlineData("Fred:Corwill", "FRED CORWILL")]
-        [InlineData("Wilfred:Corwill", "WILFRED CORWILL")]
-        [InlineData("Barney:Tornbull", "BARNEY TORNBULL")]
-        [InlineData("Betty:Tornbull", "BETTY TORNBULL")]
-        [InlineData("Bjorn:Tornbull", "BJORN TORNBULL")]
-        [InlineData("Rachel:Corwill", "RACHEL CORWILL")]
-        [InlineData("Alfred:Corwill", "ALFRED CORWILL")]
-        public void CreateMeeting_GivenASingleName_ShouldCapitaliseNameAndRemoveColon(string initialName, string expectedResult)
+        [InlineData("Fred Corwill", "FRED CORWILL")]
+        [InlineData("Wilfred Corwill", "WILFRED CORWILL")]
+        [InlineData("Barney Tornbull", "BARNEY TORNBULL")]
+        [InlineData("Betty Tornbull", "BETTY TORNBULL")]
+        [InlineData("Bjorn Tornbull", "BJORN TORNBULL")]
+        [InlineData("Rachel Corwill", "RACHEL CORWILL")]
+        [InlineData("Alfred Corwill", "ALFRED CORWILL")]
+        public void CapitaliseName_GivenASingleName_ShouldCapitaliseName(string initialName, string expectedResult)
         {
-            var actualResult = MeetingOrganiser.CreateMeeting(initialName);
+            var actualResult = MeetingOrganiser.CapitaliseName(initialName);
             Assert.Equal(expectedResult, actualResult);
         }
+        
+        
         [Theory]
-        [InlineData("Fred:Corwill", "CORWILL FRED")]
-        [InlineData("Wilfred:Corwill", "CORWILL WILFRED")]
-        [InlineData("Barney:Tornbull", "TORNBULL BARNEY")]
-        [InlineData("Betty:Tornbull", "TORNBULL BETTY")]
-        [InlineData("Bjorn:Tornbull", "TORNBULL BJORN")]
-        [InlineData("Rachel:Corwill", "CORWILL RACHEL")]
-        [InlineData("Alfred:Corwill", "CORWILL ALFRED")]
-        public void CreateMeeting_GivenASingleName_ShouldSwapSurnameAndName(string initialName, string expectedResult)
+        [InlineData("FRED CORWILL", "CORWILL FRED")]
+        [InlineData("WILFRED CORWILL", "CORWILL WILFRED")]
+        [InlineData("BARNEY TORNBULL", "TORNBULL BARNEY")]
+        [InlineData("BETTY TORNBULL", "TORNBULL BETTY")]
+        [InlineData("BJORN TORNBULL", "TORNBULL BJORN")]
+        [InlineData("RACHEL CORWILL", "CORWILL RACHEL")]
+        [InlineData("ALFRED CORWILL", "CORWILL ALFRED")]
+        public void ReverseNames_GivenASingleName_ShouldSwapSurnameAndName(string initialName, string expectedResult)
+        {
+            var actualResult = MeetingOrganiser.ReverseNames(initialName);
+            Assert.Equal(expectedResult, actualResult);
+        }
+        
+        [Theory]
+        [InlineData("Fred:Corwill", "(CORWILL, FRED)")]
+        [InlineData("Wilfred:Corwill", "(CORWILL, WILFRED)")]
+        [InlineData("Barney:Tornbull", "(TORNBULL, BARNEY)")]
+        [InlineData("Betty:Tornbull", "(TORNBULL, BETTY)")]
+        [InlineData("Bjorn:Tornbull", "(TORNBULL, BJORN)")]
+        [InlineData("Rachel:Corwill", "(CORWILL, RACHEL)")]
+        [InlineData("Alfred:Corwill", "(CORWILL, ALFRED)")]
+        public void CreateMeeting_GivenASingleName_ShouldReturnFormattedCapitalisedName(string initialName, string expectedResult)
         {
             var actualResult = MeetingOrganiser.CreateMeeting(initialName);
             Assert.Equal(expectedResult, actualResult);
