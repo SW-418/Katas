39ec299b836e29b096bf9d2bfd506c89795f4585
Sam Wells <samwells95@gmail.com>
2019-02-19 18:26:55 +0000

    Add initial solution

---
+++ Meeting/Intial/Meeting/Meeting/MeetingOrganiser.cs
@@ -0,0 +1,10 @@
+namespace Meeting
+{
+    public static class MeetingOrganiser
+    {
+        public static string CreateMeeting(string names)
+        {
+            return names;
+        }
+    }
+}
