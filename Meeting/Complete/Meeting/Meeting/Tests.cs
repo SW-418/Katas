c45b79a357827d71978cb97b06bb35b923fd9542
Sam Wells <samwells95@gmail.com>
2019-02-19 18:31:35 +0000

    Copy initial solution to completed directory

---
+++ Meeting/Complete/Meeting/Meeting/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Meeting
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData("Fred:Corwill", "FRED CORWILL")]
+        [InlineData("Wilfred:Corwill", "WILFRED CORWILL")]
+        [InlineData("Barney:Tornbull", "BARNEY TORNBULL")]
+        public void Meeting_GivenASingleName_ShouldCapitaliseNameAndRemoveColon(string initialName, string expectedResult)
+        {
+            var actualResult = MeetingOrganiser.CreateMeeting(initialName);
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
