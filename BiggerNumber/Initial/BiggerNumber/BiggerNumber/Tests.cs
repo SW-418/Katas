7c9e5a2d757525c5b0988f7cddf4096be6dc5d28
Sam Wells <samwells95@gmail.com>
2019-02-21 18:49:50 +0000

    Add initial solution with failing tests

---
+++ BiggerNumber/Initial/BiggerNumber/BiggerNumber/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace BiggerNumber
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void BiggerNumber_ReturnsMinus1_WhenSingleDigit(long number)
+        {
+            var result = Calculator.BiggerNumber(number);
+            Assert.Equal(-1, result);
+        }
+    }
+
+    public class Calculator
+    {
+        public static long BiggerNumber(long number)
+        {
+            return number;
+        }
+    }
+}
