1382bee4bb3faeabdb460d51a4bb98c20d245659
Sam Wells <samwells95@gmail.com>
2019-02-23 14:32:25 +0000

    add failing test fo recurring digits

--- BiggerNumber/Complete/BiggerNumber/BiggerNumber/Tests.cs
+++ BiggerNumber/Complete/BiggerNumber/BiggerNumber/Tests.cs
@@ -20,5 +20,21 @@ namespace BiggerNumber
             var result = Calculator.BiggerNumber(number);
             Assert.Equal(-1, result);
         }
+        
+        [Theory]
+        [InlineData(111)]
+        [InlineData(22)]
+        [InlineData(33333)]
+        [InlineData(44)]
+        [InlineData(55555)]
+        [InlineData(666)]
+        [InlineData(777)]
+        [InlineData(888)]
+        [InlineData(999)]
+        public void BiggerNumber_ReturnsMinus1_WhenNumberIsRecurringDigits(long number)
+        {
+            var result = Calculator.BiggerNumber(number);
+            Assert.Equal(-1, result);
+        }
     }
 }
