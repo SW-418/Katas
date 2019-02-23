a5b9d5721651af9446f4bad9fe6750079e326036
Sam Wells <samwells95@gmail.com>
2019-02-23 14:43:19 +0000

    pass failing test

--- BiggerNumber/Complete/BiggerNumber/BiggerNumber/Calculator.cs
+++ BiggerNumber/Complete/BiggerNumber/BiggerNumber/Calculator.cs
@@ -11,6 +11,23 @@ namespace BiggerNumber
             {
                 return -1;
             }
+
+
+            var numberString = number.ToString();
+            var firstNumber = numberString[0];
+            var recurringNumbers = 0;
+            for (var i = 1; i < numberLength; i++)
+            {
+                if (firstNumber == numberString[i])
+                {
+                    recurringNumbers++;
+                }
+            }
+
+            if (recurringNumbers == numberLength - 1)
+            {
+                return -1;
+            }
             return number;
         }
     }
